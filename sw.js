d0279080d249bb324c42590e235a2a2739714656
Alberto Iribe Gonzalez <jaigalbert@gmail.com>
2026-09-15 12:12:43 -0700

    Update service worker to cache additional assets

--- sw.js
+++ sw.js
@@ -1,10 +1,38 @@
 const CACHE_NAME = 'dossier160-v1';
-const urlsToCache = [ './', './index.html', './manifest.json' ];
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/diseno.css',
+  './js/core.js',
+  './assets/icono.png'
+];
 
+// Instalación y almacenamiento en caché de todos los archivos de la app
 self.addEventListener('install', event => {
-  event.waitUntil( caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)) );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
+// Limpieza de versiones anteriores del cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Estrategia: Cargar desde la caché primero, si no existe va a la red
 self.addEventListener('fetch', event => {
-  event.respondWith( caches.match(event.request).then(response => response || fetch(event.request)) );
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
